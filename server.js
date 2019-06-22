d5b015b6dc0daf718a965f3caa5d676786b955b3
rush <rushastronomo0415@gmail.com>
2019-06-22 09:07:06 -0700

    ready to deploy

--- server.js
+++ server.js
@@ -32,19 +32,6 @@ app.get('/contact', (req, res) => {
     console.log(msg);
     sgMail.send(msg);
   });
-//   app.post('/thanks', (req, res) => { 
-//     res.render('thanks', { contact: req.body })
-// sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-// const contact = req.body;
-// const msg = {
-//   to: 'rushastronomo0415@gmail.com',
-//   from: 'test@example.com',
-//   subject: `${contact.firstName} ${contact.lastName}`,
-//   text: 'test',
-//   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
-// };
-// sgMail.send(msg);
-//   });   
 
 app.get('/',(req, res) => { 
     const data = {
